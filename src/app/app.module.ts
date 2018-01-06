6da45601ad2244bda8c6347b9a0872e3e2bee078
William <william.xiang1375@gmail.com>
2018-01-06 14:28:29 -0500

    Configuring Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule, MatToolbar } from '@angular/material';
 
+import 'hammerjs'
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
